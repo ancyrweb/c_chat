1f3f5c15ac9ce767b553b8f5406ce91c5b9d9cf5
Benkhebbab Anthony <rewieer@gmail.com>
2018-06-24 13:44:31 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/mytchat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mytchat.dir/main.c.o"
+  "mytchat.pdb"
+  "mytchat"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mytchat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/mytchat.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mytchat.
+# This may be replaced when dependencies are built.
